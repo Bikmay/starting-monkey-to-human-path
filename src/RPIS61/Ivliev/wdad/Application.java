14211152cca257a4051247f7cad84e84fc072023
Bikmay <artyom.ivliev@gmail.com>
2018-10-04 23:41:55 +0400

    Added entry point

---
+++ src/RPIS61/Ivliev/wdad/Application.java
@@ -0,0 +1,8 @@
+package RPIS61.Ivliev.wdad;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("I'm Artyom Ivliev, and I'm not a mmonkey");
+    }
+}
